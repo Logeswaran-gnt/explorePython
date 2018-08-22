fb350bab0fb097a4443797ba8b34bd496558801b
Logeswaran G <logeswarangnt@gmail.com>
2018-08-22 15:51:26 +0530

    Create gmailSupport.py

---
+++ gmailSupport.py
@@ -0,0 +1,87 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+
+def SendMail():
+    msg = MIMEMultipart()
+    msg['Subject'] = 'Your Mail Subject'
+    msg['From'] = 'XXX@mail.com'  #set from mail-id
+    msg['To'] = 'XXX@mail.com' # set to mail-id
+
+
+    #Prepare a html formatted text to send
+    text = MIMEText('''
+<html>
+<head>
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: green;
+}
+</style>
+</head>
+<body>
+
+<table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+  <tr>
+    <td>Ernst Handel</td>
+    <td>Roland Mendel</td>
+    <td>Austria</td>
+  </tr>
+  <tr>
+    <td>Island Trading</td>
+    <td>Helen Bennett</td>
+    <td>UK</td>
+  </tr>
+  <tr>
+    <td>Laughing Bacchus Winecellars</td>
+    <td>Yoshi Tannamuri</td>
+    <td>Canada</td>
+  </tr>
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td>Giovanni Rovelli</td>
+    <td>Italy</td>
+  </tr>
+</table>
+
+</body>
+</html>''',"html")
+    msg.attach(text)
+
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.ehlo()
+    s.starttls()
+    s.ehlo()
+    s.login("XXX@gmail.com", "password")
+    s.sendmail(msg['From'],msg["To"], msg.as_string())
+    s.quit()
+
+SendMail()
